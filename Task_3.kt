046fcf3fddf13a50e9f8823f4d222ff21f71db3b
Yanuarsyah Pratama <yanuarsyahpratama@gmail.com>
2025-03-14 17:16:29 +0700

    Update Task_3.kt

--- Task_3.kt
+++ Task_3.kt
@@ -2,6 +2,14 @@ fun main(){
     print("Masukkan string bracket: ")
     val input = readLine()?.replace(" ","")?: return println("Input tidak valid")
     println(isBalancedBracket(input))
+
+    print("Masukkan string bracket: ")
+    val input2 = readLine()?.replace(" ","")?: return println("Input tidak valid")
+    println(isBalancedBracket(input2))
+
+    print("Masukkan string bracket: ")
+    val input3 = readLine()?.replace(" ","")?: return println("Input tidak valid")
+    println(isBalancedBracket(input3))
 }
 
 private fun isBalancedBracket(s: String): String{
